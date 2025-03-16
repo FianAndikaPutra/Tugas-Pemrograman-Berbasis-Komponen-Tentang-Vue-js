c4ad7f194f2db7087ad79b0a942a9d8685f13c33
FianAndikaPutra <fianandikaputra@student.uir.ac.id>
2025-03-16 23:35:59 +0700

    Add files via upload

---
+++ tugas4.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+import { ref } from 'vue'
+
+const angka = ref()
+let hasil = ref()
+const isClicked = ref(false)
+const Ganti = ref(true)
+
+function hitung() {
+    hasil.value = new Function(`return ${angka.value}`)();
+    
+}
+
+function Gantiwarna() {
+    isClicked.value = !isClicked.value
+}
+
+function toggle() {
+    Ganti.value = !Ganti.value
+}
+
+</script>
+
+<template>
+
+<input type ="text" placeholder="masukkan angka" v-model="angka" >
+<button @click="hitung">Hitung</button>
+
+<br><br>
+
+<div>{{ angka }}</div>
+<div>{{ hasil }}</div>
+
+<h1 :class="isClicked ? 'title-Clicked' : 'title'">HELLO WORLD</h1>
+<button @click="Gantiwarna">Ganti Warna</button>
+
+<button @click="toggle">Toggle</button>
+<h1 v-if="Ganti">BENAR</h1>
+<h1 v-else>SALAH</h1>
+
+
+</template>
+
+<style scoped>
+.title {
+    color: red;
+}
+.title-Clicked {
+    color: blue;
+}
+</style>
